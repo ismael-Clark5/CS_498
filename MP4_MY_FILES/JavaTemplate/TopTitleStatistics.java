df3956df09eb39cb41bf40ccd35349a51dce343a
ismael-clark5 <iclar013@fiu.edu>
2023-02-11 15:36:15 -0500

    Casting values to ints 4

--- MP4_MY_FILES/JavaTemplate/TopTitleStatistics.java
+++ MP4_MY_FILES/JavaTemplate/TopTitleStatistics.java
@@ -196,12 +196,12 @@ public class TopTitleStatistics extends Configured implements Tool {
                 sum += value;
             }
             var = 0;
-            mean = int(Math.floor(sum / countToTitleMap.size()));
+            mean = (int)Math.floor(sum / countToTitleMap.size());
             for(Pair<Integer, String> item : countToTitleMap) {
                 Integer value = new Integer(item.first);
-                var += int(Math.floor(Math.pow(value - mean, 2)));
+                var += (int)Math.floor(Math.pow(value - mean, 2));
             }
-            var = int(Math.floor(Math.sqrt(var/ countToTitleMap.size())));
+            var = (int)Math.floor(Math.sqrt(var/ countToTitleMap.size()));
 
             context.write(new Text("Mean"), new IntWritable(mean));
             context.write(new Text("Sum"), new IntWritable(sum));
