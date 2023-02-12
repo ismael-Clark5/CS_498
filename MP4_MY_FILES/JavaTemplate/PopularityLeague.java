7af729f5d0f2c6c8f17cc5bf72fc9328348bad6d
ismael-clark5 <iclar013@fiu.edu>
2023-02-11 19:48:45 -0500

    Part E first commit

--- MP4_MY_FILES/JavaTemplate/PopularityLeague.java
+++ MP4_MY_FILES/JavaTemplate/PopularityLeague.java
@@ -169,7 +169,8 @@ public class PopularityLeague extends Configured implements Tool {
                 IntWritable link = new IntWritable(Integer.parseInt(pair[0].toString()));
                 IntWritable count = new IntWritable(Integer.parseInt(pair[1].toString()));
                 for(IntArrayWritable val1 : values){
-                    IntWritable count2 = new IntWritable(Integer.parseInt(pair[1].toString()));
+                    IntWritable[] pair1 =(IntWritable[]) val1.toArray();
+                    IntWritable count2 = new IntWritable(Integer.parseInt(pair1[1].toString()));
                     if(count.compareTo(count2) > 0){
                         rank += 1;
                     }
