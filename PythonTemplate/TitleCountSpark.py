e45d7dc8a03de0aae5befb024933367caabc152f
ismael-clark5 <iclar013@fiu.edu>
2023-02-17 18:13:03 -0500

    testing again

--- PythonTemplate/TitleCountSpark.py
+++ PythonTemplate/TitleCountSpark.py
@@ -22,7 +22,7 @@ lines = sc.textFile(sys.argv[3], 1)
 words = lines.flatMap(lambda line: line.split(delimiters))
 
 wordCounts = words.map(lambda word: (word, 1) if (word not in stopwords) else None).reduceByKey(lambda a,b:a +b)
-top10Lists = wordCounts.sortBy(lambda x :(-x[1], x[0])).cahce().take(10)
+top10Lists = wordCounts.sortBy(lambda x :(-x[1], x[0])).cache().take(10)
 outputFile = open(sys.argv[4],"w")
 for finalWord in top10Lists:
     outputFile.write(finalWord + "\n")
