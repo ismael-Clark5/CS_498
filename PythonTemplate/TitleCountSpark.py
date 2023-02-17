24b286cadd358aea4d2e114f39f86b06af0e51b0
ismael-clark5 <iclar013@fiu.edu>
2023-02-17 18:23:13 -0500

    testing again

--- PythonTemplate/TitleCountSpark.py
+++ PythonTemplate/TitleCountSpark.py
@@ -4,7 +4,7 @@
 
 import sys
 from pyspark import SparkConf, SparkContext
-import re
+
 stopWordsPath = sys.argv[1]
 delimitersPath = sys.argv[2]
 
@@ -19,7 +19,7 @@ conf.set("spark.driver.bindAddress", "127.0.0.1")
 sc = SparkContext(conf=conf)
 
 lines = sc.textFile(sys.argv[3], 1)
-words = lines.flatMap(lambda line: re.split(delimiters, line.strip()))
+words = lines.flatMap(lambda line: line.strip().split(delimiters))
 
 wordCounts = words.map(lambda word: (word, 1) if (word not in stopwords) else None).reduceByKey(lambda a,b:a +b)
 top10Lists = wordCounts.sortBy(lambda x :(-x[1], x[0])).cache().take(10)
@@ -28,8 +28,5 @@ for finalWord in top10Lists:
     word = finalWord[0]
     count = finalWord[1]
     outputFile.write(word + " " + str(count) + "\n")
-
-#TODO
-#write results to output file. Foramt for each line: (line +"\n")
 
 sc.stop()
