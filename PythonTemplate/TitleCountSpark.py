985a5c66cabb016c7db45f9f30abf855b01e96b6
ismael-clark5 <iclar013@fiu.edu>
2023-02-17 18:17:11 -0500

    testing again

--- PythonTemplate/TitleCountSpark.py
+++ PythonTemplate/TitleCountSpark.py
@@ -19,7 +19,7 @@ conf.set("spark.driver.bindAddress", "127.0.0.1")
 sc = SparkContext(conf=conf)
 
 lines = sc.textFile(sys.argv[3], 1)
-words = lines.flatMap(lambda line: line.split(delimiters))
+words = lines.flatMap(lambda line: line.strip().split(delimiters))
 
 wordCounts = words.map(lambda word: (word, 1) if (word not in stopwords) else None).reduceByKey(lambda a,b:a +b)
 top10Lists = wordCounts.sortBy(lambda x :(-x[1], x[0])).cache().take(10)
