65b2154384a4e68ce0410b46610b312946b76980
ismael-clark5 <iclar013@fiu.edu>
2023-02-18 11:55:15 -0500

    Sat work

--- PythonTemplate/TopTitleStatisticsSpark.py
+++ PythonTemplate/TopTitleStatisticsSpark.py
@@ -15,7 +15,7 @@ minimum = words.reduce(lambda a, b : min(int(a), int(b)))
 maximum = words.reduce(lambda a,b : max(int(a), int(b)))
 sum = words.reduce(lambda a, b :(int(a) + int(b)))
 mean = sum / words.count()
-var = words.map(lambda a: math.floor(pow((a - mean), 2)))
+var = words.map(lambda a: math.floor(pow((float(a) - mean), 2)))
 var = var.reduce(lambda a, b :(int(a) + int(b))) / words.count()
 
 outputFile = open(sys.argv[2], "w")
