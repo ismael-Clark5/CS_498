c46510215fec547ab2c98ea2249108390c53f5a3
ismael-clark5 <iclar013@fiu.edu>
2023-02-18 10:22:30 -0500

    Sat work

--- PythonTemplate/TopTitleStatisticsSpark.py
+++ PythonTemplate/TopTitleStatisticsSpark.py
@@ -9,15 +9,22 @@ sc = SparkContext(conf=conf)
 lines = sc.textFile(sys.argv[1], 1)
 
 words = lines.flatMap(lambda line: line.split(" ")).filter(lambda x : x.isnumeric())
+# mean = words
+sum = words.reduce(lambda a,b :( a + b))
+minimum = words.reduce(lambda a,b : min(a, b))
+maximum = words.reduce(lambda a,b : max(a, b))
+# var =
 words.saveAsTextFile("./output")
 outputFile = open(sys.argv[2], "w")
+outputFile.write('Sum\t%s\n' % sum)
+outputFile.write('Min\t%s\n' % minimum)
+outputFile.write('Max\t%s\n' % maximum)
+
 '''
 TODO write your output here
 write results to output file. Format
 outputFile.write('Mean\t%s\n' % ans1)
-outputFile.write('Sum\t%s\n' % ans2)
-outputFile.write('Min\t%s\n' % ans3)
-outputFile.write('Max\t%s\n' % ans4)
+
 outputFile.write('Var\t%s\n' % ans5)
 '''
 
