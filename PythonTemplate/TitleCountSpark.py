b2ac8b2652cd5736fdfef56d416688bf3d31ac77
ismael-clark5 <iclar013@fiu.edu>
2023-02-18 14:38:41 -0500

    ...

--- PythonTemplate/TitleCountSpark.py
+++ PythonTemplate/TitleCountSpark.py
@@ -38,11 +38,11 @@ lines = sc.textFile(sys.argv[3], 1)
 delimiterAsRe = re.compile('[' + re.escape(delimiters) + ']')
 words = lines.flatMap(lambda line: delimiterAsRe.split(line.lower().strip()))
 wordCounts = words.filter(lambda x: x not in stopwords).map(lambda word: (word, 1)).reduceByKey(lambda a,b:a +b).sortBy(lambda x : (-x[1], x[0])).take(10)
-
+wordCounts = sorted(wordCounts, key=lambda x: x[0])
 outputFile = open(sys.argv[4],"w")
 for finalWord in wordCounts:
     word = finalWord[0]
     count = finalWord[1]
-    outputFile.write(word + " " + str(count) + "\n")
+    outputFile.write(word + "\t" + str(count) + "\n")
 
 sc.stop()
