90e6027bfd51b77c1aa17a30ff03f8f9aff23195
ismael-clark5 <iclar013@fiu.edu>
2023-02-18 13:32:28 -0500

    Sat work

--- PythonTemplate/TopPopularLinksSpark.py
+++ PythonTemplate/TopPopularLinksSpark.py
@@ -6,14 +6,15 @@ conf = SparkConf().setMaster("local").setAppName("TopPopularLinks")
 conf.set("spark.driver.bindAddress", "127.0.0.1")
 sc = SparkContext(conf=conf)
 
-lines = sc.textFile(sys.argv[1], 1) 
-
-#TODO
-
+lines = sc.textFile(sys.argv[1], 100)
+linksTo =  lines.map(lambda line: line.strip().split(":")[1])
+linksTo = linksTo.flatMap(lambda line: line.strip().split(" "))
+linkCounts = linksTo.map(lambda word: (word, 1)).reduceByKey(lambda a,b:a +b).sortBy(lambda x : (-x[1], x[0])).take(10).sortBy(lambda x : x[0])
 output = open(sys.argv[2], "w")
-
-#TODO
-#write results to output file. Foramt for each line: (key + \t + value +"\n")
+for finalWord in linkCounts:
+    word = finalWord[0]
+    count = finalWord[1]
+    linkCounts.write(word + " " + str(count) + "\n")
 
 sc.stop()
 
