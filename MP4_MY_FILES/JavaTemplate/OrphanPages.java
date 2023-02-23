af1183964fe8759ca23e7b0da8f733b0323e4b2b
ismael-clark5 <iclar013@fiu.edu>
2023-02-23 18:51:13 -0500

    Last time in Java

--- MP4_MY_FILES/JavaTemplate/OrphanPages.java
+++ MP4_MY_FILES/JavaTemplate/OrphanPages.java
@@ -51,7 +51,7 @@ public class OrphanPages extends Configured implements Tool {
             String[] line = value.toString().trim().split(":");
             String pageId = line[0];
             String[] links = line[1].trim().split(" ");
-            context.write(pageId, new IntWritable(0));
+            context.write(new IntWritable(Integer.parseInt(pageId)), new IntWritable(0));
             for(int i = 0; i < links.length; i++){
                 if(!pageId.equals(links[i])){
                     context.write(new IntWritable(Integer.parseInt(links[i])), new IntWritable(1));
