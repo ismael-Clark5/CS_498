942a13e363d513488be3fb6ff83319e299745f4a
ismael-clark5 <iclar013@fiu.edu>
2023-03-19 15:49:05 -0400

    Part G

--- MP7_HBase_Template/JavaTemplate/phoenix.sql
+++ MP7_HBase_Template/JavaTemplate/phoenix.sql
@@ -2,7 +2,7 @@ CREATE VIEW "powers" (pk VARCHAR PRIMARY KEY, "personal"."hero" VARCHAR, "person
 "professional"."name" VARCHAR, "professional"."xp" VARCHAR, "custom".color VARCHAR);
 
 SELECT p.name as Name1, p1.name as Name2, p.power as Power
-FROM powers as p
+FROM "powers" as p
 INNER JOIN powers as p1
     ON p.power = p1.power
     AND p.hero = "yes" and p1.hero = "yes";
