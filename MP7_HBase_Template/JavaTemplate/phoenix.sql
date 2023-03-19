1374e12a6fd50e738aeb2242dd56f1ecceff0741
ismael-clark5 <iclar013@fiu.edu>
2023-03-19 15:54:08 -0400

    Part G

--- MP7_HBase_Template/JavaTemplate/phoenix.sql
+++ MP7_HBase_Template/JavaTemplate/phoenix.sql
@@ -2,10 +2,10 @@
 --DROP TABLE IF EXISTS "powers";
 --
 --CREATE VIEW "powers" (pk VARCHAR PRIMARY KEY, "personal"."hero" VARCHAR, "personal"."power" VARCHAR,
---"professional"."name" VARCHAR, "professional"."xp" VARCHAR, "custom".color VARCHAR);
+--"professional"."name" VARCHAR, "professional"."xp" VARCHAR, "custom"."color" VARCHAR);
 
 SELECT p.name as Name1, p1.name as Name2, p.power as Power
 FROM "powers" as p
 INNER JOIN "powers" as p1
-    ON p.power = p1.power
-    AND p.hero = "yes" and p1.hero = "yes";
+    ON p."power" = p1."power"
+    AND p."hero" = "yes" and p1."hero" = "yes";
