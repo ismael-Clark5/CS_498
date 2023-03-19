7d9f149815f70138e530495611c54b7a4013fcec
ismael-clark5 <iclar013@fiu.edu>
2023-03-19 16:03:09 -0400

    Part G

--- MP7_HBase_Template/JavaTemplate/phoenix.sql
+++ MP7_HBase_Template/JavaTemplate/phoenix.sql
@@ -5,5 +5,5 @@
 SELECT p."name" as Name1, p1."name" as Name2, p."power" as Power
 FROM "powers" as p
 INNER JOIN "powers" as p1
-    ON p."power" = p1."power"
-    AND p."hero" = yes and p1."hero" = yes;
+ON p."power" = p1."power"
+WHERE p."hero" = "yes" and p1."hero" = "yes";
