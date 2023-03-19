bbef23ea9a9dff49696488a4e2dbedecc75222cd
ismael-clark5 <iclar013@fiu.edu>
2023-03-19 13:38:27 -0400

    Part C

--- MP7_HBase_Template/JavaTemplate/TablePartC.java
+++ MP7_HBase_Template/JavaTemplate/TablePartC.java
@@ -27,13 +27,48 @@ public class TablePartC{
 
    public static void main(String[] args) throws IOException {
 
-	File csvInput = new File("input.csv");
+    HBaseConfiguration hBaseConfig = new HBaseConfiguration(new Configuration());
+    HBaseAdmin hba = new HBaseAdmin(hBaseConfig);
+    HTable hTable = new HTable(hBaseConfig, "powers");
+    HColumnDescriptor powersPersonalDescriptor = new HColumnDescriptor("personal");
+    HColumnDescriptor powersProfessionalDescriptor = new HColumnDescriptor("professional");
+    HColumnDescriptor powersCustomDescriptor = new HColumnDescriptor("custom");
+
+    hba.addColumn("hero", powersPersonalDescriptor);
+    hba.addColumn("power", powersPersonalDescriptor);
+
+    hba.addColumn("name", powersProfessionalDescriptor);
+    hba.addColumn("xp", powersProfessionalDescriptor);
+
+    hba.addColumn("color", powersCustomDescriptor);
+
+    File csvInput = new File("input.csv");
     FileReader fr = new FileReader(csvInput);
     BufferedReader br = new BufferedReader(fr);
     String line = "";
+    String delimiter = ",";
+    Put p;
     while((line = br.readLine()) != null){
-        System.out.println(line);
+        String [] elements = line.strip().split(delimiter);
+        String rowName = elements[0];
+        String hero = elements[1];
+        String power = elements[2];
+        String name = elements[3];
+        String xp = elements[4];
+        String color = elements[5];
+
+        p = new Put(Bytes.toBytes(rowName));
+        p.add(Bytes.toBytes("personal"), Bytes.toBytes("hero"), Bytes.toBytes(hero));
+        p.add(Bytes.toBytes("personal"), Bytes.toBytes("power"), Bytes.toBytes(power));
+
+        p.add(Bytes.toBytes("professional"), Bytes.toBytes("name"), Bytes.toBytes(name));
+        p.add(Bytes.toBytes("professional"), Bytes.toBytes("xp"), Bytes.toBytes(name));
+
+        p.add(Bytes.toBytes("custom"), Bytes.toBytes("color"), Bytes.toBytes(color));
+
+        hTable.put(p);
     }
+    hTable.close();
    }
 }
 
