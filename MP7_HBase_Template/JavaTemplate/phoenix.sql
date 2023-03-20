66b2e7346c8d0a8fbeab76a806d31a78c41ac0ca
ismael-clark5 <iclar013@fiu.edu>
2023-03-20 18:55:02 -0400

    Part G

--- MP7_HBase_Template/JavaTemplate/phoenix.sql
+++ MP7_HBase_Template/JavaTemplate/phoenix.sql
@@ -1,9 +1,8 @@
-DROP VIEW "powers";
---CREATE VIEW "powers" (pk VARCHAR PRIMARY KEY, "personal"."hero" VARCHAR, "personal"."power" VARCHAR,
---"professional"."name" VARCHAR, "professional"."xp" VARCHAR, "custom"."color" VARCHAR);
---
---SELECT p."name" as Name1, p1."name" as Name2, p."power" as Power
---FROM "powers" as p
---JOIN "powers" as p1
---ON p."power" = p1."power"
---WHERE p."hero" = 'yes' and p1."hero" = 'yes';
+CREATE VIEW "powers" (pk VARCHAR PRIMARY KEY, "personal"."hero" VARCHAR, "personal"."power" VARCHAR,
+"professional"."name" VARCHAR, "professional"."xp" VARCHAR, "custom"."color" VARCHAR);
+
+SELECT p."name" as Name1, p1."name" as Name2, p."power" as Power
+FROM "powers" as p
+JOIN "powers" as p1
+ON p."power" = p1."power"
+WHERE p."hero" = 'yes' and p1."hero" = 'yes';
