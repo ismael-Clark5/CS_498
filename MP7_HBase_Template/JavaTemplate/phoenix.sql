1341269a43d88e05992555b583189d26fdeb0b04
ismael-clark5 <iclar013@fiu.edu>
2023-03-20 18:33:20 -0400

    Part G

--- MP7_HBase_Template/JavaTemplate/phoenix.sql
+++ MP7_HBase_Template/JavaTemplate/phoenix.sql
@@ -1,7 +1,7 @@
 --DROP VIEW "powers";
 --SELECT * FROM "powers"
-CREATE VIEW "powers" (pk VARCHAR PRIMARY KEY, "personal"."hero" VARCHAR, "personal"."power" VARCHAR,
-"professional"."name" VARCHAR, "professional"."xp" VARCHAR, "custom"."color" VARCHAR);
+--CREATE VIEW "powers" (pk VARCHAR PRIMARY KEY, "personal"."hero" VARCHAR, "personal"."power" VARCHAR,
+--"professional"."name" VARCHAR, "professional"."xp" VARCHAR, "custom"."color" VARCHAR);
 --
 --SELECT p."name" as Name1, p1."name" as Name2, p."power" as Power
 --FROM "powers" as p
